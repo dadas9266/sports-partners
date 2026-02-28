f2d5ab2e5e50231a9867f864b7657b5a7703f217
Yusuf Kucukugurlu <yusuf.kucukugurlu@gmail.com>
2026-02-28 03:10:26 +0300

    chore: update dependencies, tsconfig and add vscode tasks

--- prisma.config.ts
+++ prisma.config.ts
@@ -4,7 +4,4 @@ import { defineConfig } from "prisma/config";
 
 export default defineConfig({
   schema: path.join(__dirname, "prisma", "schema.prisma"),
-  datasource: {
-    url: process.env.DATABASE_URL!,
-  },
 });
